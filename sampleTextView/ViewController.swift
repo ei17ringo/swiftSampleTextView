f998ab299a8da8a4b2c8f41cf5878e3f67c4d75c
ei17ringo <ei17ringo@gmail.com>
2016-01-28 17:56:19 +0800

    commit init

--- sampleTextView/ViewController.swift
+++ sampleTextView/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextView: UITextView!
+    
+    @IBOutlet weak var btnClose: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTextView.text = "Hello"
+        myTextView.textColor = UIColor.blueColor()
+        myTextView.font = UIFont(name: "AmericanTypewriter", size: 20)
+        myTextView.textAlignment = NSTextAlignment.Center
+        
+        // TODO:Hello10個出す
+        for var i=1; i <= 10; i++ {
+            myTextView.text = myTextView.text + "Hello"
+        }
+        
+        //TODO: こんにちはnで10個出す
+        var n = 0
+        while (n < 10){
+            myTextView.text = myTextView.text + "こんにちは\(n)"
+            n++
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapClose(sender: UIButton) {
+        
+        //キーボードを閉じる
+        myTextView.resignFirstResponder()
+    }
 
 }
 
